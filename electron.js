50bd7a19e6414dba4630062f0d5483707166e12e
Jonathan MARTIN <j.martin@lonsdale.fr>
2020-10-12 19:17:00 +0200

    first

---
+++ electron.js
@@ -0,0 +1,191 @@
+const fs = require('fs');
+const { ipcMain, app, BrowserWindow, Menu } = require('electron')
+const path = require('path')
+const url = require('url')
+
+
+// Gardez une reference globale de l'objet window, si vous ne le faites pas, la fenetre sera
+// fermee automatiquement quand l'objet JavaScript sera garbage collected.
+let win;
+
+
+function createWindow() {
+  
+    // Créer le browser window.
+    win = new BrowserWindow({
+        width: 1800,
+        height: 1000,
+        title: "Email editor",
+        frame: true,
+        resizable: true,
+        transparent: false,
+        icon: path.join('file://', __dirname, "/assets/icon/icon.ico")
+    }); 
+    win.loadURL(path.join('file://', __dirname, '/index.html'))
+    // et charge le index.html de l'application.
+    win.loadFile('index.html')
+
+    ipcMain.on('save', (event, arg) => {
+        //console.log(arg) // affiche "ping"
+        event.returnValue = 'pong'
+        /*fs.writeFile('ff.txt', 'Hello World!', function (err) {
+          if (err) 
+              return console.log(err);
+          console.log('Wrote Hello World in file helloworld.txt, just check it');
+      
+      
+        });*/
+    })
+
+    const template = [
+        {
+            label: 'Custom',
+            submenu: [
+                { role: 'undo' },
+                { role: 'redo' },
+                { type: 'separator' },
+                { role: 'cut' },
+                { role: 'copy' },
+                { role: 'paste' },
+                { role: 'pasteandmatchstyle' },
+                { role: 'delete' },
+                { role: 'selectall' }
+            ]
+        },
+        {
+            label: 'Edit',
+            submenu: [
+                { role: 'undo' },
+                { role: 'redo' },
+                { type: 'separator' },
+                { role: 'cut' },
+                { role: 'copy' },
+                { role: 'paste' },
+                { role: 'pasteandmatchstyle' },
+                { role: 'delete' },
+                { role: 'selectall' }
+            ]
+        },
+        {
+            label: 'View',
+            submenu: [
+                { role: 'reload' },
+                { role: 'forcereload' },
+                { role: 'toggledevtools' },
+                { type: 'separator' },
+                { role: 'resetzoom' },
+                { role: 'zoomin' },
+                { role: 'zoomout' },
+                { type: 'separator' },
+                { role: 'togglefullscreen' }
+            ]
+        },
+        {
+            role: 'window',
+            submenu: [
+                { role: 'minimize' },
+                { role: 'close' }
+            ]
+        },
+        {
+            role: 'help',
+            submenu: [
+                {
+                    label: 'Learn More',
+                    click() { require('electron').shell.openExternal('https://electronjs.org') }
+                }
+            ]
+        }
+    ]
+
+    if (process.platform === 'darwin') {
+        template.unshift({
+            label: app.getName(),
+            submenu: [
+                { role: 'about' },
+                { type: 'separator' },
+                { role: 'services' },
+                { type: 'separator' },
+                { role: 'hide' },
+                { role: 'hideothers' },
+                { role: 'unhide' },
+                { type: 'separator' },
+                { role: 'quit' }
+            ]
+        })
+
+        // Edit menu
+        template[1].submenu.push(
+            { type: 'separator' },
+            {
+                label: 'Speech',
+                submenu: [
+                    { role: 'startspeaking' },
+                    { role: 'stopspeaking' }
+                ]
+            }
+        )
+
+        // Window menu
+        template[3].submenu = [
+            { role: 'close' },
+            { role: 'minimize' },
+            { role: 'zoom' },
+            { type: 'separator' },
+            { role: 'front' }
+        ]
+    }
+
+    const menu = Menu.buildFromTemplate(template)
+
+    Menu.setApplicationMenu(menu)
+    const dockMenu = Menu.buildFromTemplate([
+        {
+            label: 'New Window',
+            click() { console.log('New Window') }
+        }, {
+            label: 'New Window with Settings',
+            submenu: [
+                { label: 'Basic' },
+                { label: 'Pro' }
+            ]
+        },
+        { label: 'New Command...' }
+    ])
+
+    app.dock.setMenu(dockMenu)
+    //app.setName("lll")
+
+    // Ouvre les DevTools.
+    win.webContents.openDevTools()
+
+    // Émit lorsque la fenêtre est fermée.
+    win.on('closed', () => {
+        // Dé-référence l'objet window , normalement, vous stockeriez les fenêtres
+        // dans un tableau si votre application supporte le multi-fenêtre. C'est le moment
+        // où vous devez supprimer l'élément correspondant.
+        win = null
+    })
+}
+
+// Cette méthode sera appelée quant Electron aura fini
+// de s'initialiser et sera prêt à créer des fenêtres de navigation.
+// Certaines APIs peuvent être utilisées uniquement quand cet événement est émit.
+app.on('ready', createWindow)
+
+// Quitte l'application quand toutes les fenêtres sont fermées.
+app.on('window-all-closed', () => {
+    // Sur macOS, il est commun pour une application et leur barre de menu
+    // de rester active tant que l'utilisateur ne quitte pas explicitement avec Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    // Sur macOS, il est commun de re-créer une fenêtre de l'application quand
+    // l'icône du dock est cliquée et qu'il n'y a pas d'autres fenêtres d'ouvertes.
+    if (win === null) {
+        createWindow()
+    }
+})
